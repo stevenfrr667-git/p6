759f77b7fd83decfc11f4cffe88837b1cff06888
Steven Ferreira <steven.ferreira2001@gmail.com>
2025-10-24 10:29:28 +0200

    commit maj css btn and footer

---
+++ FrontEnd/script.js
@@ -0,0 +1,15 @@
+const res = await fetch("http://localhost:5678/api/works");
+const gallery = document.querySelector(".gallery");
+
+const data = [
+    blablabla API    
+];
+
+function createFigure  
+    const figure = document.createElement("figure");
+
+    const img = document.createElement("img");
+   
+    const figcaption = document.createElement("figcaption");
+    figcaption.textContent = 
+
